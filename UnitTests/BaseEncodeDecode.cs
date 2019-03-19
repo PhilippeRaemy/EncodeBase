3cb68358e263c2a10af7e05de3dd4c6846d9e293
Philippe Raemy <philippe_raemy@swissonline.ch>
2019-03-19 14:57:06 +0100

    Update class to test at once with various code strings and bytes arrays of various lengths

--- UnitTests/BaseEncodeDecode.cs
+++ UnitTests/BaseEncodeDecode.cs
@@ -6,26 +6,33 @@ namespace UnitTests
     using Xunit;
     using EncodeBase;
 
-    public class Bytes : TheoryData<byte>
-    {
-        public Bytes()
-        {
-            for (byte b = 0; b <= 255; b++)
-                Add(b);
-        }
-    }
-
     public class BaseEncodeDecode
     {
-        public static IEnumerable<object[]> Bytes => Enumerable.Range(0, 255).Select(i => new object []{i});
+        static readonly string [] Codes = {
+            "01", 
+            "0123",
+            "01234567",
+            "0123456789abcdef",
+            "0123456789abcdefghijklmnopqrstuv",
+        };
 
+        static readonly byte[] Bytes = Enumerable.Range(0, 255).Select(b => (byte)b).ToArray();
+
+        static readonly int[] Prefixes = Enumerable.Range(0, 8).ToArray();
+
+        public static IEnumerable<object[]> Cases =>
+            from c in Codes
+            from b in Bytes
+            from p in Prefixes
+            select new object[] {c, b, p};
 
         [Theory]
-        [MemberData(nameof(Bytes))]
-        public void OneByte(byte b)
+        [MemberData(nameof(Cases))]
+        public void EncodeDecode(string code, byte b, int prefix)
         {
-            var bytes = new[] {b};
-            Assert.Equal(bytes, bytes.EncodeBase("01234567").ToArray().DecodeBase("01234567").ToArray());
+            var bytes = Bytes.Take(prefix).Append(b).ToArray();
+            Assert.Equal(bytes, bytes.EncodeBase(code).ToArray().DecodeBase(code).ToArray());
         }
+
     }
 }
