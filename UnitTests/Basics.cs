f4f95286342880be3d1fa18705a374214c9a0915
Philippe Raemy <philippe_raemy@swissonline.ch>
2019-03-19 16:26:36 +0100

    Basic test (bytes order)

---
+++ UnitTests/Basics.cs
@@ -0,0 +1,15 @@
+﻿namespace UnitTests
+{
+    using System.Linq;
+    using EncodeBase;
+    using Xunit;
+
+    public class Basics
+    {
+        [Fact]
+        public void CaseOxFF()
+        {
+            Assert.Equal("ZW", new string(new byte[]{255}.EncodeBase(CodingStrings.Base32).ToArray()));
+        }
+    }
+}
